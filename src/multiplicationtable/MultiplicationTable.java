3153c702ce15a2e490d8c2dddea77ae3897c1710
Vladislav Myzov <mozov.vlad@mail.ru>
2021-09-28 21:06:18 +0300

    Создали таблицу умножения и вывели результат на экран

--- src/multiplicationtable/MultiplicationTable.java
+++ src/multiplicationtable/MultiplicationTable.java
@@ -3,12 +3,13 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MultiplicationTable {
-    
+    // Создаем три цвета
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String BLACK = "\033[0m";
     
     public static void main(String[] args) {
+        // Создаем переменную правильных ответов и присваеваем ей значение нуля, то же самое далем с а, б и попытками
         int correct_answers = 0;
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
@@ -16,24 +17,28 @@ public class MultiplicationTable {
         int b = 0;
         int tries = 0;
         
+        // Пока попытки не равны нуля, мы запускаем наш цикл while и закоплняем переменные а и б случайными числами
         while(tries != 5) {
             a = random.nextInt(9)+1;
             b = random.nextInt(9)+1;
             System.out.print(a + "*" + b + "=");
-            int UserInput = scanner.nextInt();
-            tries = tries + 1;
+            int UserInput = scanner.nextInt(); // Пользователь вводит ответ на пример
+            tries = tries + 1; // Попытки увеличиваются
+            // Если ответ верен, то мы окрашиваем надпись "Ваш ответ верен" в зеленый цвет и увеличиваем кол-во верных ответов на 1
             if(UserInput == a * b) {
                 System.out.println(ANSI_GREEN + "Ваш ответ верен!" + BLACK);
                 correct_answers = correct_answers + 1;
                 
             }
+            // Если же неверный, то выводим надпись "Ваш ответ неверен" красного цвета и не прибавляем верных ответов
             if(UserInput != a * b) {
                 System.out.println(ANSI_RED + "Ваш ответ неверен!" + BLACK);           
                 
             }
             
         }
-        
+        // В конце, исходя из числа верных ответов, мы выводим соответсвующий текст похвалы, 
+        // либо же наоборот указываем на то, что надо учить таблицу умножения
         if(correct_answers == 5) {
             System.out.println("Молодец! Ты отлично знаешь таблицу умножения!");
             System.exit(0);
